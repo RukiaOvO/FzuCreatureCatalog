166c3b32bbf4710ed02a94fb1ac5b5138d6605e4
RukiaOvO <1403555427@qq.com>
2024-10-28 00:47:17 +0800

    finish: jwt-Create and jwt-Parse function.

---
+++ common/src/main/java/com/catalog/utils/JwtUtil.java
@@ -0,0 +1,40 @@
+package com.catalog.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtil
+{
+    public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {
+        // 指定签名算法
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        // 生成JWT的时间
+        long expMillis = System.currentTimeMillis() + ttlMillis;
+        Date exp = new Date(expMillis);
+        // 设置jwt的body
+        JwtBuilder builder = Jwts.builder()
+                // 设置声明
+                .setClaims(claims)
+                // 设置签名使用的签名算法和签名使用的秘钥
+                .signWith(signatureAlgorithm, secretKey.getBytes(StandardCharsets.UTF_8))
+                // 设置过期时间
+                .setExpiration(exp);
+        return builder.compact();
+    }
+
+    public static Claims parseJWT(String secretKey, String token) {
+        // 得到默认的JwtParser
+        Claims claims = Jwts.parser()
+                // 设置签名的秘钥
+                .setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+                // 设置需要解析的jwt
+                .parseClaimsJws(token).getBody();
+        return claims;
+    }
+}
